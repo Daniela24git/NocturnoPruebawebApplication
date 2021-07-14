70e0fb18a1603d6d5a67f04fdf418ac9657215f4
Daniela24git <danielatorresdanixx2014@hotmail.com>
2021-07-13 23:31:23 -0500

    Applicationweb

---
+++ Controllers/ConsultasController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationPrueba.Data;
+using WebApplicationPrueba.Entities;
+
+namespace WebApplicationPrueba.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        readonly ApplicationDbContext _context;
+        public ConsultasController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+
+        }
+
+        public IEnumerable<Departamento> Departamentos()
+        {
+            return _context.Departamentos;
+             
+        }
+
+        public IEnumerable<Empleado> Empleados()
+        {
+            //return _context.Empleados.OrderBy(x => x.Apellido);
+
+            //return _context.Empleados.OrderByDescending(x => x.Apellido);
+
+            //return _context.Empleados.OrderBy(x => x.Nombre).ThenBy(x => x.Apellido);
+
+            var empleados = _context.Empleados.ToList();
+            return empleados.OrderBy(e => e.NombreCompleto);
+        }
+    }
+}
